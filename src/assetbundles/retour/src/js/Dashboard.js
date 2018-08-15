a18d45a18998553984117698bfd5911d10475447
Andrew Welch <andrew@nystudio107.com>
2018-08-15 19:36:30 -0400

    Vue
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/src/js/Dashboard.js
+++ src/assetbundles/retour/src/js/Dashboard.js
@@ -1,22 +1,23 @@
-import Vue from 'vue';
-
 const main = async () => {
     const apexCharts = await import(/* webpackChunkName: "apexcharts" */ 'apexcharts');
     console.log("Dashboard ApexCharts hello!");
 
+    const Vue = await import(/* webpackChunkName: "vue" */ 'vue');
+    console.log("Dashboard Vue hello!");
+
+    const vm = new Vue({
+        el: "#dash",
+        components: {
+            'confetti': () => import(/* webpackChunkName: "confetti" */ '../vue/Confetti.vue')
+        },
+        delimiters: ["${", "}"],
+        data: {
+            message: "hello from vue!",
+        },
+        methods: {
+        },
+    });
 };
 
 main();
 
-const vm = new Vue({
-    el: "#dash",
-    components: {
-        'confetti': () => import(/* webpackChunkName: "confetti" */ '../vue/Confetti.vue')
-    },
-    delimiters: ["${", "}"],
-    data: {
-        message: "hello from vue!",
-    },
-    methods: {
-    },
-});
