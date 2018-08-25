e8b4539a13300b30c24e6b638a08a6481ae69706
Andrew Welch <andrew@nystudio107.com>
2018-08-25 01:20:14 -0400

    Add vue-apexcharts
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/src/js/Dashboard.js
+++ src/assetbundles/retour/src/js/Dashboard.js
@@ -1,28 +1,20 @@
-// Load in Apex charts
-const loadCharts = async () => {
-    const apexCharts = await import(/* webpackChunkName: "apexcharts" */ 'apexcharts');
-    console.log("Dashboard ApexCharts hello!");
-};
-
 // Dashboard main
 const main = async () => {
     // Async load the vue module
     const Vue = await import(/* webpackChunkName: "vue" */ 'vue');
-    console.log("Dashboard hello, world");
     // Create our vue instance
     const vm = new Vue.default({
         el: "#cp-nav-content",
         delimiters: ["${", "}"],
         components: {
-            'confetti': () => import(/* webpackChunkName: "confetti" */ '../vue/Confetti.vue')
+            'confetti': () => import(/* webpackChunkName: "confetti" */ '../vue/Confetti.vue'),
+            'dashboard-chart': () => import(/* webpackChunkName: "dashboard-chart" */ '../vue/DashboardChart.vue')
         },
         data: {
         },
         methods: {
         },
         mounted() {
-            loadCharts().then({
-            });
         }
     });
 };
---
+++ src/assetbundles/retour/src/vue/DashboardChart.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <apexcharts width="100%" type="line" :options="chartOptions" :series="series"></apexcharts>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import VueApexCharts from 'vue-apexcharts'
+
+    Vue.use(VueApexCharts);
+
+    export default {
+        components: {
+            apexcharts: VueApexCharts,
+        },
+        data: function() {
+            return {
+                chartOptions: {
+                    chart: {
+                        id: 'vuechart-example'
+                    },
+                    xaxis: {
+                        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                    }
+                },
+                series: [{
+                    name: 'series-1',
+                    data: [30, 40, 45, 50, 49, 60, 70, 91]
+                }]
+            }
+        },
+    }
+</script>
