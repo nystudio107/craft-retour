f0a2956757313a73d6129a28f66c6125c541e876
Andrew Welch <andrew@nystudio107.com>
2018-08-31 23:50:48 -0400

    Add Settings model & config.php
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/config.php
+++ src/config.php
@@ -24,8 +24,27 @@
  */
 
 return [
-
     // The public facing name of the plugin
     'pluginName' => 'Retour',
 
+    // Controls whether Retour automatically creates static redirects when an entry's URI changes.
+    'createUriChangeRedirects' => true,
+
+    // Should the query string be stripped from all 404 URLs before their evaluation?
+    'alwaysStripQueryString'   => false,
+
+    // How many static redirects to display in the Admin CP
+    'staticRedirectDisplayLimit'  => 100,
+
+    // How many dynamic redirects to display in the Admin CP
+    'dynamicRedirectDisplayLimit' => 100,
+
+    // Should the query string be stripped from the saved statistics source URLs?
+    'stripQueryStringFromStats'   => true,
+
+    // How many stats should be stored
+    'statsStoredLimit' => 1000,
+
+    // How many stats to display in the Admin CP
+    'statsDisplayLimit' => 1000,
 ];
--- src/models/Settings.php
+++ src/models/Settings.php
@@ -31,6 +31,41 @@ class Settings extends Model
      */
     public $pluginName = 'Retour';
 
+    /**
+     * @var bool Controls whether Retour automatically creates static redirects when an entry's URI changes.
+     */
+    public $createUriChangeRedirects = true;
+
+    /**
+     * @var bool Should the query string be stripped from all 404 URLs before their evaluation?
+     */
+    public $alwaysStripQueryString = false;
+
+    /**
+     * @var int How many static redirects to display in the Admin CP
+     */
+    public $staticRedirectDisplayLimit = 100;
+
+    /**
+     * @var int How many dynamic redirects to display in the Admin CP
+     */
+    public $dynamicRedirectDisplayLimit = 100;
+
+    /**
+     * @var bool Should the query string be stripped from the saved statistics source URLs?
+     */
+    public $stripQueryStringFromStats = true;
+
+    /**
+     * @var int How many stats should be stored
+     */
+    public $statsStoredLimit = 1000;
+
+    /**
+     * @var int How many stats to display in the Admin CP
+     */
+    public $statsDisplayLimit = 1000;
+
     // Public Methods
     // =========================================================================
 
@@ -42,6 +77,22 @@ class Settings extends Model
         return [
             ['pluginName', 'string'],
             ['pluginName', 'default', 'value' => 'Retour'],
+            [
+                [
+                    'createUriChangeRedirects',
+                    'alwaysStripQueryString',
+                    'stripQueryStringFromStats',
+                ],
+                'boolean'
+            ],
+            ['staticRedirectDisplayLimit', 'integer', 'min' => 1],
+            ['staticRedirectDisplayLimit', 'default', 'value' => 100],
+            ['dynamicRedirectDisplayLimit', 'integer', 'min' => 1],
+            ['dynamicRedirectDisplayLimit', 'default', 'value' => 100],
+            ['statsStoredLimit', 'integer', 'min' => 1],
+            ['statsStoredLimit', 'default', 'value' => 1000],
+            ['statsDisplayLimit', 'integer', 'min' => 1],
+            ['statsDisplayLimit', 'default', 'value' => 1000],
         ];
     }
 }
