1306afae48353c1f8e441ae5a2e2d2fce68865c3
Andrew Welch <andrew@nystudio107.com>
2018-11-05 23:23:50 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/dist/js/styles.js
+++ src/assetbundles/retour/dist/js/styles.js
@@ -2,8 +2,8 @@
  * @project        Retour
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Mon, Nov 5, 2018 3:54 AM ET
- * @release        3336d4feb2714480095614476a1e42859f24ed18 [develop]
+ * @build          Tue, Nov 6, 2018 4:21 AM ET
+ * @release        2bc8b35ae136bc8d6d2b8eb3f27981feb81bf579 [develop]
  * @copyright      Copyright (c) 2018 nystudio107
  *
  */
