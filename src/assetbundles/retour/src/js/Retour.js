61b73b625fc8605e45601735f4602bfa9f6ada47
Andrew Welch <andrew@nystudio107.com>
2019-02-04 01:40:08 -0500

    Add Widget mask icon
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/src/js/Retour.js
+++ src/assetbundles/retour/src/js/Retour.js
@@ -1,4 +1,4 @@
 // Import our CSS
 import styles from '../css/app.pcss';
 import icon from '../img/Retour-icon.svg';
-import icon from '../img/icon-mask.svg';
+import widgetIcon from '../img/icon-mask.svg';
