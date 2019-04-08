57bc07a9c12ea8434e835ad832151b0d36a47b51
Andrew Welch <andrew@nystudio107.com>
2019-04-08 11:48:50 -0400

    Add EnvAttributeParserBehavior for future use on Craft 31
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -11,7 +11,10 @@
 
 namespace nystudio107\retour\models;
 
+use nystudio107\retour\Retour;
+
 use craft\base\Model;
+use craft\behaviors\EnvAttributeParserBehavior;
 use craft\validators\ArrayValidator;
 
 use yii\behaviors\AttributeTypecastBehavior;
@@ -125,11 +128,22 @@ class Settings extends Model
      */
     public function behaviors()
     {
-        return [
+        $craft31Behaviors = [];
+        if (Retour::$craft31) {
+            $craft31Behaviors = [
+                'parser' => [
+                    'class' => EnvAttributeParserBehavior::class,
+                    'attributes' => [
+                    ],
+                ]
+            ];
+        }
+
+        return array_merge($craft31Behaviors, [
             'typecast' => [
                 'class' => AttributeTypecastBehavior::class,
                 // 'attributeTypes' will be composed automatically according to `rules()`
             ],
-        ];
+        ]);
     }
 }
