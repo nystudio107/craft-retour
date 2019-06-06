df23c855a3b61d786099e6e3fbd600e08e3a47f5
Andrew Welch <andrew@nystudio107.com>
2019-06-05 20:36:51 -0400

    Add module.hot.accept() to entry points
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/src/js/Redirects.js
+++ src/assetbundles/retour/src/js/Redirects.js
@@ -21,3 +21,8 @@ const vm = new Vue({
         this.$events.$on('refresh-table', eventData => this.onTableRefresh(eventData));
     },
 });
+
+// Accept HMR as per: https://webpack.js.org/api/hot-module-replacement#accept
+if (module.hot) {
+    module.hot.accept();
+}
