64ed5b6d6beea6ce03261dd4a5061f29b36b65b5
Andrew Welch <andrew@nystudio107.com>
2019-06-12 00:52:27 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/dist/js/styles.js
+++ src/assetbundles/retour/dist/js/styles.js
@@ -2,10 +2,10 @@
  * @project        Retour
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Wed, Jun 12, 2019 3:34 AM ET
- * @release        823ba640da9bc0b1e385da9f5799964c7cf44665 [feature/tailwind1.0.0]
+ * @build          Wed, Jun 12, 2019 3:50 AM ET
+ * @release        0bb0c7f860a3247e79f4732dff2496e1702e3e6f [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{152:function(n,w,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{153:function(n,w,o){}}]);
 //# sourceMappingURL=styles.js.map
