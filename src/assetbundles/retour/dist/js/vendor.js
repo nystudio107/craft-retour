f893a3ccdfb738a5bbdedb67354be75c4626dcac
Andrew Welch <andrew@nystudio107.com>
2019-09-27 09:10:15 -0400

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/dist/js/vendor.js
+++ src/assetbundles/retour/dist/js/vendor.js
@@ -2,8 +2,8 @@
  * @project        Retour
  * @name           vendor.js
  * @author         Andrew Welch
- * @build          Sun, Sep 8, 2019 12:43 AM ET
- * @release        0604cba3b547af2a99d8ef967cc8c6edd7d38980 [develop]
+ * @build          Fri, Sep 27, 2019 1:09 PM ET
+ * @release        9102e0cc4abddbf01f6916429b7a56f15587e4c4 [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
