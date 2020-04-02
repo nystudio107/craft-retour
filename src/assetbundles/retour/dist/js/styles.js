e83cf35b8e5fcac227f671bde0f323037316c2de
Andrew Welch <andrew@keluli.local>
2020-04-02 12:51:44 -0400

    Revert "Updated assets build"
    This reverts commit d0807711c4806bfc03cb70457c7cf0f6ff36946c.

--- src/assetbundles/retour/dist/js/styles.js
+++ src/assetbundles/retour/dist/js/styles.js
@@ -2,9 +2,9 @@
  * @project        Retour
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Thu, Apr 2, 2020 4:28 PM ET
- * @release        af2ce5319d5358204596e46ac82809d56e7feadd [develop]
- * @copyright      Copyright (c) 2020 nystudio107
+ * @build          Mon, Nov 11, 2019 11:27 PM ET
+ * @release        39e7ad60ee5e660e385a2a71154bde2436d94a6a [develop]
+ * @copyright      Copyright (c) 2019 nystudio107
  *
  */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{76:function(n,w,o){}}]);
