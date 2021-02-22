93cd10fcaf1eb23e6238a9971fc7dbcb4a37558d
Andrew Welch <andrew@nystudio107.com>
2021-02-22 18:50:47 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/retour/dist/js/retour.js
+++ src/assetbundles/retour/dist/js/retour.js
@@ -2,7 +2,7 @@
  * @project        retour
  * @name           retour.js
  * @author         Andrew Welch
- * @build          Thu Feb 18 2021 04:42:17 GMT+0000 (Coordinated Universal Time)
+ * @build          Mon Feb 22 2021 23:50:01 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2021 ©2020 nystudio107.com
  *
  */
