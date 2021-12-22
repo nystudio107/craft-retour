4ca0fc6d3a2f36dd921521e39e04fb976b70ac2c
Andrew Welch <andrew@nystudio107.com>
2021-12-22 16:30:04 -0500

    feat: Update to use Tailwind CSS `^3.0.0`

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,26 +1,11 @@
 // module exports
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      '../src/templates/**/*.{twig,html}',
-      '../src/assetbundles/retour/src/vue/**/*.{vue,html}',
-      './node_modules/vuetable-2/src/components/**/*.{vue,html}',
-    ],
-    layers: [
-      'base',
-      'components',
-      'utilities',
-    ],
-    mode: 'layers',
-    options: {
-      whitelist: [
-        '../src/assetbundles/retour/src/css/components/*.css',
-      ],
-    }
-  },
+  content: [
+    '../src/templates/**/*.{twig,html}',
+    '../src/assetbundles/retour/src/vue/**/*.{vue,html,js}',
+    './node_modules/vuetable-2/src/components/**/*.{vue,html}',
+  ],
   theme: {
   },
-  corePlugins: {},
   plugins: [],
 };
