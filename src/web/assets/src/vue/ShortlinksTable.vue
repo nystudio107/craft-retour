6056be815da927896d5e464d5e95e2796beebe07
Andrew Welch <andrew@nystudio107.com>
2022-08-26 17:27:05 -0400

    refactor: Better spacing on the cog dropdown

--- src/web/assets/src/vue/ShortlinksTable.vue
+++ src/web/assets/src/vue/ShortlinksTable.vue
@@ -25,7 +25,7 @@
         >{{ stringRedirects }}</span>:</label>
         <div className="btngroup inline">
           <div
-            className="btn menubtn"
+            className="ml-2 btn menubtn"
             data-icon="settings"
           />
           <div
@@ -49,7 +49,7 @@
       :initial-filter-text="filterText"
     />
     <div className="vuetable-pagination clearafter">
-      <vuetable-pagination-info ref="paginationInfoTop" />
+      <vuetable-pagination-info ref="paginationInfoTop"/>
 
       <div className="floated left vuetable-pagination-info py-3">
         <div className="inline pl-3 text-gray-600">
@@ -99,7 +99,7 @@
       @vuetable:pagination-data="onPaginationData"
     />
     <div className="vuetable-pagination clearafter border-solid">
-      <vuetable-pagination-info ref="paginationInfo" />
+      <vuetable-pagination-info ref="paginationInfo"/>
       <vuetable-pagination
         ref="pagination"
         @vuetable-pagination:change-page="onChangePage"
