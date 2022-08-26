34691d226bea6e921854b4c0b6991cc49d4199b3
Andrew Welch <andrew@nystudio107.com>
2022-08-25 23:34:05 -0400

    refactor: Add Shortlinks & declare Craft global

--- src/web/assets/src/js/@types/shims.d.ts
+++ src/web/assets/src/js/@types/shims.d.ts
@@ -2,4 +2,7 @@ declare module "Dashboard";
 declare module "Import";
 declare module "Redirects";
 declare module "Retour";
+declare module "Shortlinks";
 declare module "Widget";
+
+declare var Craft: Object;
