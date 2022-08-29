c0f3c72c9abf7e8c0fad03b8bcf3c47bd8374dca
Hector D. Byrd <hector@ambrosia.net>
2022-08-29 08:57:29 +0300

    chore: Fix Short Link field behavior.

--- src/fields/ShortLink.php
+++ src/fields/ShortLink.php
@@ -89,7 +89,6 @@ class ShortLink extends Field implements PreviewableFieldInterface
         }
 
         $value = $element->getFieldValue($this->handle);
-        RetourPlugin::$plugin->redirects->removeElementRedirect($element);
 
         // Return for propagating elements
         if ($this->redirectSrcMatch === 'pathonly') {
@@ -100,6 +99,8 @@ class ShortLink extends Field implements PreviewableFieldInterface
             $siteUrl = $element->getSite()->getBaseUrl();
             $value = rtrim($siteUrl, '/') . '/' . ltrim($value, '/');
         }
+
+        RetourPlugin::$plugin->redirects->removeElementRedirect($element, $this->redirectSrcMatch === 'pathonly');
 
         if (!empty($value)) {
             RetourPlugin::$plugin->redirects->enableElementRedirect($element, $value, $this->redirectSrcMatch, $this->redirectHttpCode);
