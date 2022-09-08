a4de8f49f02c39930e6d1ea8d8cdbf78092f6e0d
Andrew Welch <andrew@nystudio107.com>
2022-09-08 00:30:31 -0400

    refactor: Include "Short Link Element ID" in the CSV redirects export

--- src/web/assets/src/vue/ImportDraggable.vue
+++ src/web/assets/src/vue/ImportDraggable.vue
@@ -19,7 +19,7 @@
             class="retour-import-list-group-item whitespace-no-wrap clearafter"
             :class="{'retour-empty-item': element.name === '' }"
           >
-            <a class="move icon mr-2" />
+            <a class="move icon mr-2"/>
             <span
               class="truncate inline-block leading-tight align-bottom"
               style="width: 80%;"
@@ -120,6 +120,7 @@ export default {
         Craft.t('retour', 'Site ID'),
         Craft.t('retour', 'Legacy URL Match Type'),
         Craft.t('retour', 'Hits'),
+        Craft.t('retour', 'Short Link Element ID'),
       ]
     }
   },
