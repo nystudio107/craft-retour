2263f6cebe86a45781ce5577e75eeb76644dc1e5
Andrew Welch <andrew@nystudio107.com>
2022-10-24 22:48:10 -0400

    refactor: Add `import.meta.hot.accept` for entrypoint JavaScript for HMR

--- src/web/assets/src/js/Redirects.js
+++ src/web/assets/src/js/Redirects.js
@@ -5,16 +5,23 @@ import RedirectsTable from '@/vue/RedirectsTable.vue';
 Vue.use(VueEvents);
 // Create our vue instance
 new Vue({
-    el: "#cp-nav-content",
-    components: {
-        'redirects-table': RedirectsTable,
-    },
-    mounted() {
-        this.$events.$on('refresh-table', eventData => this.onTableRefresh(eventData));
-    },
-    methods: {
-        onTableRefresh (vuetable) {
-            Vue.nextTick( () => vuetable.refresh());
-        }
-    },
+  el: "#cp-nav-content",
+  components: {
+    'redirects-table': RedirectsTable,
+  },
+  mounted() {
+    this.$events.$on('refresh-table', eventData => this.onTableRefresh(eventData));
+  },
+  methods: {
+    onTableRefresh(vuetable) {
+      Vue.nextTick(() => vuetable.refresh());
+    }
+  },
 });
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log("HMR")
+  });
+}
