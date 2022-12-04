d8d5353aa9a535ab57c1eec6794bad46e59e320b
Andrew Welch <andrew@nystudio107.com>
2022-12-03 23:24:54 -0500

    fix: Fix deleted `sort-order` prop

--- src/web/assets/src/vue/RedirectsTable.vue
+++ src/web/assets/src/vue/RedirectsTable.vue
@@ -49,7 +49,7 @@
       :initial-filter-text="filterText"
     />
     <div class="vuetable-pagination clearafter">
-      <vuetable-pagination-info ref="paginationInfoTop" />
+      <vuetable-pagination-info ref="paginationInfoTop"/>
 
       <div class="floated left vuetable-pagination-info py-3">
         <div class="inline pl-3 text-gray-600">
@@ -95,10 +95,11 @@
       :css="css"
       :fields="fields"
       :per-page="perPage"
+      :sort-order="sortOrder"
       @vuetable:pagination-data="onPaginationData"
     />
     <div class="vuetable-pagination clearafter border-solid">
-      <vuetable-pagination-info ref="paginationInfo" />
+      <vuetable-pagination-info ref="paginationInfo"/>
       <vuetable-pagination
         ref="pagination"
         @vuetable-pagination:change-page="onChangePage"
