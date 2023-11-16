b0b7a312d793b5f153381db9e2c170978391f9d7
Andrew Welch <andrew@nystudio107.com>
2023-11-16 17:03:21 -0500

    feat: Switch over to Vite `^5.0.0` & Node `^20.0.0` for the buildchain

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({command}) => ({
   base: command === 'serve' ? '' : '/dist/',
   build: {
     emptyOutDir: true,
-    manifest: true,
+    manifest: 'manifest.json',
     outDir: '../src/web/assets/dist',
     rollupOptions: {
       input: {
