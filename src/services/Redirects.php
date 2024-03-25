8fe60a1092580bff68dd0eb9e4792c638509f2c2
Andrew Welch <andrew@nystudio107.com>
2024-03-25 09:50:19 -0400

    fix: Fixed an issue where an empty redirect in the `excludePatterns` list could cause redirects to stop functioning, add logging when a redirect is excluded ([#297](https://github.com/nystudio107/craft-retour/issues/297))

--- src/services/Redirects.php
+++ src/services/Redirects.php
@@ -16,6 +16,7 @@ use craft\base\Component;
 use craft\base\ElementInterface;
 use craft\base\Plugin;
 use craft\db\Query;
+use craft\errors\ElementNotFoundException;
 use craft\errors\SiteNotFoundException;
 use craft\helpers\Db;
 use craft\helpers\StringHelper;
@@ -28,6 +29,7 @@ use nystudio107\retour\helpers\Text as TextHelper;
 use nystudio107\retour\helpers\UrlHelper;
 use nystudio107\retour\models\StaticRedirects as StaticRedirectsModel;
 use nystudio107\retour\Retour;
+use Throwable;
 use yii\base\ExitException;
 use yii\base\InvalidConfigException;
 use yii\base\InvalidRouteException;
@@ -273,9 +275,21 @@ class Redirects extends Component
         $uri = '/' . ltrim($uri, '/');
         if (!empty(Retour::$settings->excludePatterns)) {
             foreach (Retour::$settings->excludePatterns as $excludePattern) {
+                if (empty($excludePattern['pattern'])) {
+                    continue;
+                }
                 $pattern = '`' . $excludePattern['pattern'] . '`i';
                 try {
                     if (preg_match($pattern, $uri) === 1) {
+                        Craft::info(
+                            Craft::t(
+                                'retour',
+                                'Excluded URI: {uri} due to match of pattern: {pattern}',
+                                ['uri' => $uri, 'pathOnly' => $pattern]
+                            ),
+                            __METHOD__
+                        );
+
                         return true;
                     }
                 } catch (\Exception $e) {
@@ -1042,8 +1056,8 @@ class Redirects extends Component
      * Delete a short link by its ID.
      *
      * @param int $redirectId
-     * @throws \Throwable
-     * @throws \craft\errors\ElementNotFoundException
+     * @throws Throwable
+     * @throws ElementNotFoundException
      * @throws \yii\base\Exception
      */
     public function deleteShortlinkById(int $redirectId): bool
