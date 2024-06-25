d43b5e00179d58a27e8500794c5d036216af8d47
Andrew Welch <andrew@nystudio107.com>
2024-06-25 10:42:23 -0400

    fix: Fix a `ParseError` exception that is thrown by older versions of PHP that do not support types for class properties ([#307](https://github.com/nystudio107/craft-retour/issues/307))

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -60,7 +60,7 @@ class Settings extends Model
     /**
      * @var bool Should `no-cache` headers be set on the redirect response to prevent client-side caching?
      */
-    public bool $setNoCacheHeaders = true;
+    public $setNoCacheHeaders = true;
 
     /**
      * @var bool Should the anonymous ip address of the client causing a 404 be
