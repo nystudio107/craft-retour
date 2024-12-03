4db8d446f72d00bc5a600e54dea80b7c0f48c4f7
Andrew Welch <andrew@nystudio107.com>
2024-12-03 13:36:27 -0500

    fix: Don’t try to find a site destination for full URLs ([#316](https://github.com/nystudio107/craft-retour/issues/316))

--- src/services/Redirects.php
+++ src/services/Redirects.php
@@ -829,7 +829,7 @@ class Redirects extends Component
                         if ($siteId !== null) {
                             $siteId = (int)$siteId;
                         }
-                        if (!UrlHelper::pathHasSitePrefix($path)) {
+                        if (!UrlHelper::isFullUrl($dest) && !UrlHelper::pathHasSitePrefix($path)) {
                             $dest = UrlHelper::siteUrl('/', null, null, $siteId);
                             $dest = UrlHelper::mergeUrlWithPath($dest, $path);
                             $dest = parse_url($dest, PHP_URL_PATH);
