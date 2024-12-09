1c044cd5ac27d19d12626e27aeb54e5c6290a963
Andrew Welch <andrew@nystudio107.com>
2024-12-09 13:29:38 -0500

    fix: fix: Fixed an issue where deprecation errors would occur when an element does not have a `uri` ([#317](https://github.com/nystudio107/craft-retour/issues/317))

--- src/Retour.php
+++ src/Retour.php
@@ -325,7 +325,7 @@ class Retour extends Plugin
                 if (self::$settings->createUriChangeRedirects && $checkElementSlug) {
                     // Make sure this isn't a transitioning temporary draft/revision and that it's
                     // not propagating to other sites
-                    if (!str_contains($element->uri, '__temp_')) {
+                    if ($element->uri && !str_contains($element->uri, '__temp_')) {
                         Retour::$plugin->events->stashElementUris($element);
                     }
                 }
