6fd0bcf440259f2264d672aae3384b10d32de287
Andrew Welch <andrew@nystudio107.com>
2025-01-23 14:22:03 -0500

    refactor: Allow cross-origin requests -- https://github.com/vitejs/vite/security/advisories/GHSA-vg6x-rcgg-rjx6

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -65,8 +65,14 @@ export default defineConfig(({command}) => ({
     preserveSymlinks: true,
   },
   server: {
+    // Allow cross-origin requests -- https://github.com/vitejs/vite/security/advisories/GHSA-vg6x-rcgg-rjx6
+    allowedHosts: true,
+    cors: true,
     fs: {
       strict: false
+    },
+    headers: {
+      "Access-Control-Allow-Private-Network": "true",
     },
     host: '0.0.0.0',
     origin: 'http://localhost:' + process.env.DEV_PORT,
