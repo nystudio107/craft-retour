e7d47cae394969b8fb05e200c9865b6402af0aba
Andrew Welch <andrew@nystudio107.com>
2025-02-28 15:25:58 -0500

    refactor: Be specific to .ddev.site in cors origin

--- buildchain/vite.config.ts
+++ buildchain/vite.config.ts
@@ -83,7 +83,7 @@ export default defineConfig(({command}) => ({
     // Allow cross-origin requests -- https://github.com/vitejs/vite/security/advisories/GHSA-vg6x-rcgg-rjx6
     allowedHosts: true,
     cors: {
-      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(localhost|\.local|\.test|\.site)(?::\d+)?$/
+      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(localhost|\.local|\.test|\.ddev\.site)(?::\d+)?$/
     },
     fs: {
       strict: false
