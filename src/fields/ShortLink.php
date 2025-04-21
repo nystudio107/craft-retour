5eca1d90619ec73b84acf06ba7c0b2a23d500d1b
Andrew Welch <andrew@nystudio107.com>
2025-04-21 19:30:08 -0400

    refactor: Handle drafts

--- src/fields/ShortLink.php
+++ src/fields/ShortLink.php
@@ -194,6 +194,10 @@ class ShortLink extends Field implements PreviewableFieldInterface
                 function($element) {
                     $value = $element->getFieldValue($this->handle);
                     $redirect = RetourPlugin::$plugin->getRedirects()->getRedirectByRedirectSrcUrl($value);
+                    if (method_exists($element, 'getCanonical')) {
+                        // Handle drafts
+                        $element = $element->getCanonical();
+                    }
                     if ($redirect && isset($redirect['associatedElementId'])) {
                         if ($redirect['associatedElementId'] == 0) {
                             $element->addError($this->handle, Craft::t('retour', 'A Retour redirect with this Legacy URL already exists.'));
