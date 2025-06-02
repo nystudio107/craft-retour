8555a9a636ea0aa5cdb4cf25a0ce95301904717c
Andrew Welch <andrew@nystudio107.com>
2025-06-01 23:13:25 -0400

    fix: Don't append `/`'s errantly to URLs that have fragments in them if `addTrailingSlashesToUrls` is set to `true` in `config/general.php`

--- src/controllers/RedirectsController.php
+++ src/controllers/RedirectsController.php
@@ -400,7 +400,7 @@ class RedirectsController extends Controller
     protected function addSlashToSiteUrls(string $url): string
     {
         // Make sure the URL doesn't end with a file extension, e.g.: .jpg or have a query string
-        if (!preg_match('/\.[^\/]+$/', $url) && strpos($url, '?') === false) {
+        if (!preg_match('/\.[^\/]+$/', $url) && strpos($url, '?') === false && strpos($url, '#') === false) {
             // If it's a root relative URL, assume it's a site URL
             if (UrlHelper::isRootRelativeUrl($url)) {
                 return rtrim($url, '/') . '/';
