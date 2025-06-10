04cf533d7d13e01b74020b32431f0308182ed046
Andrew Welch <andrew@nystudio107.com>
2025-06-10 15:49:40 -0700

    fix: ShortLinks no longer attempt to localize the redirect value for translated fields ([#313](https://github.com/nystudio107/craft-retour/issues/313#issuecomment-2898486055))

--- src/fields/ShortLink.php
+++ src/fields/ShortLink.php
@@ -115,13 +115,6 @@ class ShortLink extends Field implements PreviewableFieldInterface
         if (!empty($value)) {
             $redirectSrcMatch = $this->redirectSrcMatch;
 
-            if ($this->translationMethod !== Field::TRANSLATION_METHOD_NONE) {
-                if (!UrlHelper::isAbsoluteUrl($value)) {
-                    $value = UrlHelper::siteUrl($value, null, null, $parentElement->siteId);
-                    $redirectSrcMatch = 'fullurl';
-                }
-            }
-
             RetourPlugin::$plugin->redirects->enableElementRedirect($parentElement, $value, $redirectSrcMatch, $this->redirectHttpCode);
         }
 
